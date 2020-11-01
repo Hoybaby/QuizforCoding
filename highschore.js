b670ad7389a071d8ed184b41eefef7ce137a1338
Hoybaby <mbartek436@gmail.com>
2020-11-01 12:40:04 -0500

    made a functional highschool html list tracker for local storage. currently works but needs tweek

--- highschore.js
+++ highschore.js
@@ -11,6 +11,7 @@ var clear = document.getElementById("clear")
 
 var highscores = []
 
+init();
 // need to create a function to render the scores saved
 
 function renderScores() {
@@ -27,17 +28,57 @@ function renderScores() {
         hsList.append(li)
 
     }
-}
+};
 
 function init() {
     // to get stored highscores from the array we create we need to parsing the JSON string to an object
     var storedScores = JSON.parse(localStorage.getItem("highscores"));
     // the JSON.parse() methos parses a JSON string, constructing the JS value or object by the string
 
-    if (storedTodos !== null) {
+    if (storedScores !== null) {
         highscores = storedScores;
     }
     renderScores();
 };
 
-function storeScores
+function storeScores() {
+    //with this function i will stringift and set the highscores key in local storage to highscores array
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+};
+
+//as of now before this code. we have no way of calling it so we need to add event listener to the button I made on the html to tie to all together
+
+
+scoreForm.addEventListener("submit", function(event){
+    event.preventDefault(); // this preventDefault method cancels the event if it is canceable
+
+    var scoreText = initialText.value.trim(); // we are using trim method to do what it says. trim any excess spaces in the input so it doesnt recieve any undesirable answers.
+    
+    if(scoreText === "") {
+    return;
+    }
+
+    // Add new todoText to todos array, clear the input
+  highscores.push(scoreText);
+  scoreForm.value = "";
+
+  storeScores();
+  renderScores();
+});
+
+
+hsList.addEventListener("click", function(event) {
+    var element = event.target;
+  
+    // If that element is a button...
+    if (element.matches("button") === true) {
+      // Get its data-index value and remove the todo element from the list
+      var index = element.parentElement.getAttribute("data-index");
+      todos.splice(index, 1);
+  
+      // Store updated todos in localStorage, re-render the list
+      storeScores();
+      renderScores();
+    }
+  });
+  
